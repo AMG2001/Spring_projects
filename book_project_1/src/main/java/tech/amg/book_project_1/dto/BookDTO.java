ee7e20ceb54b9fe7c92caac8492572f8b1ecdb2d
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2025-05-10 12:16:59 +0200

    implement bookDTO

---
+++ book_project_1/src/main/java/tech/amg/book_project_1/dto/BookDTO.java
@@ -0,0 +1,45 @@
+package tech.amg.book_project_1.dto;
+
+public class BookDTO {
+    String title, author, category;
+    int rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public BookDTO(String title, String author, String category, int rating) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.rating = rating;
+    }
+}
